89790e4d8cdc2eb89f9f6660922e8e091cc49cbc
hsimao <e087754958@gmail.com>
2021-02-13 19:20:26 +0800

    add: 新增 enemy 移動邏輯

--- assets/scripts/components/Enemy.js
+++ assets/scripts/components/Enemy.js
@@ -4,6 +4,7 @@ cc.Class({
   extends: cc.Component,
 
   properties: {
+    velocity: 150,
     levelMap: {
       default: null,
       type: LevelMap
@@ -13,9 +14,10 @@ cc.Class({
   start() {
     this.targets = this.levelMap.tileMap.getObjectGroup("path").getObjects();
     this.targetIndex = 1;
-    const position = this.getCurrrentTargetPosition();
 
-    this.node.setPosition(position);
+    this.node.setPosition(this.getCurrrentTargetPosition());
+
+    this.move();
   },
 
   getCurrentTarget() {
@@ -44,8 +46,41 @@ cc.Class({
 
     // 取得 tile 中心座標
     const x = position.x + this.levelMap.tileWidth / 2;
-    const y = position.y + this.levelMap.tileHeight / 2;
+    const y = position.y + this.levelMap.tileWidth / 2;
 
     return cc.v2(x, y);
+  },
+
+  move() {
+    this.targetIndex++;
+    const targetPosition = this.getCurrrentTargetPosition();
+
+    if (!targetPosition) {
+      this.node.emit("finished");
+      this.node.destroy();
+      return;
+    }
+
+    // 移動完後再重新執行, 前往下個移動位置
+    this.moveTo(targetPosition).then(() => this.move());
+  },
+
+  getDistance(targetPosition) {
+    const x = Math.abs(targetPosition.x - this.node.x);
+    const y = Math.abs(targetPosition.y - this.node.y);
+
+    return Math.max(x, y);
+  },
+
+  moveTo(targetPosition) {
+    // 依據移動距離來換算均速
+    const time = this.getDistance(targetPosition) / this.velocity;
+
+    return new Promise((resolve) => {
+      const moveToAction = cc.moveTo(time, targetPosition);
+      // 移動完後 call resolve
+      const sequence = cc.sequence(moveToAction, cc.callFunc(resolve));
+      this.node.runAction(sequence);
+    });
   }
 });
--- assets/scripts/components/LevelMap.js
+++ assets/scripts/components/LevelMap.js
@@ -31,9 +31,9 @@ cc.Class({
   // 使用整個畫布 x, y 軸座標, 來換算取得在 tileMap 上的位置
   getTileCoordinatesByPosition(position) {
     return {
-      x: Math.round(position.x / this.tileWidth),
+      x: Math.floor(position.x / this.tileWidth),
       // y 軸由下往上算
-      y: this.mapHeight - Math.round(position.y / this.tileHeight) - 1
+      y: this.mapHeight - Math.floor(position.y / this.tileHeight) - 1
     };
   }
 });
