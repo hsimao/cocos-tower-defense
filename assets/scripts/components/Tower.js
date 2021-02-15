b0751d17c93b7f87bbfc5f2f86882d43a7834b30
hsimao <e087754958@gmail.com>
2021-02-15 18:05:23 +0800

    add: 新增砲台追蹤小兵位置轉動邏輯

--- assets/scripts/components/Tower.js
+++ assets/scripts/components/Tower.js
@@ -1,7 +1,10 @@
 cc.Class({
   extends: cc.Component,
 
-  properties: {},
+  properties: {
+    rotationSpeed: 500,
+    reloadTime: 0.5 // 幾秒發射子彈一次
+  },
 
   start() {
     this.targets = [];
@@ -10,6 +13,10 @@ cc.Class({
   init(coordinates) {
     this.coordinates = coordinates;
     this.targets = [];
+
+    this.schedule(() => {
+      this.tryFire();
+    }, this.reloadTime);
   },
 
   onCollisionEnter(other, self) {
@@ -30,5 +37,30 @@ cc.Class({
     return this.targets.length
       ? this.targets.find((target) => target.active)
       : false;
+  },
+
+  tryFire() {
+    const targetNode = this.getTarget();
+
+    if (targetNode && targetNode.active) {
+      const targetPosition = cc.v2(targetNode.x, targetNode.y);
+      // 砲台轉到小兵位置方向
+      this.rotateTo(targetPosition);
+    }
+  },
+
+  rotateTo(targetPosition) {
+    const angle = -this.getAngle(targetPosition);
+    const distance = Math.abs(angle - this.node.angle);
+
+    if (distance) {
+      const time = distance / this.rotationSpeed;
+      this.node.runAction(cc.rotateTo(time, angle));
+    }
+  },
+
+  // 計算要轉的角度
+  getAngle({ x, y }) {
+    return (Math.atan2(y - this.node.y, x - this.node.x) * 180) / Math.PI - 90;
   }
 });
